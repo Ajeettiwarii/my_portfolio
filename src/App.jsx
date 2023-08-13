358aacc29f04d009237886ea43086902f279abf7
ajeet <ajeettiwari.ajju21may@gmail.com>
2023-08-13 19:30:33 +0530

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Skills from './containers/skills'
 import Portfolio from './containers/portfolio'
 import Contact from './containers/contact'
 import Navbar from './components/navBar'
-import particles from './utils.js/particles';
+import particles from './utils/particles';
 function App() {     
   const location=useLocation();
   console.log(location);
---
+++ src/containers/Skills/utils.js
@@ -0,0 +1,61 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "70",
+        },   
+        {
+          skillName: "REACT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "NODE",
+          percentage: "80",
+        },
+        {
+          skillName: "EXPRESS",
+          percentage: "70",
+        },
+        {
+          skillName: "POSTMAN",
+          percentage: "60",
+        },  
+        {
+          skillName: "REST API",
+          percentage: "75",
+        },
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "75",
+        },
+        {
+          skillName: "ORACLE",
+          percentage: "85",
+        },
+      ],
+    },
+  ];
